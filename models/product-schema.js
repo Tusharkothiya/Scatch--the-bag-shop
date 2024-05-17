aa4356aebc75b130c97903e98c4c45775a2e2f13
demo <tusharkothiya330@gmail.com>
2024-05-17 09:43:32 +0530

    product model add

---
+++ models/product-schema.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+let productSchema = new mongoose.Schema({
+    image: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    discount: {
+        type: Number,
+        default: 0
+    },
+    bgcolor: {
+        type: String,
+        required: true
+    },
+    panelcolor:{
+        type: String,
+        required: true
+    },
+    textcolor: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('product', productSchema);
