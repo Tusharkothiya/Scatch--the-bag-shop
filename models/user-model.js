630cbe348916308a7cb159f74762aa8302859829
demo <tusharkothiya330@gmail.com>
2024-05-17 09:35:28 +0530

    first commit

---
+++ models/user-model.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+let userSchema = new mongoose.Schema({
+    fullname:{
+        type: String,
+        require: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        require: true,
+        trim: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    cart:{
+        type: Array,
+        default: []
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    orders: {
+        type: Array,
+        default: []
+    },
+    contact: {
+        type: Number,
+        required: true
+    },
+    picture: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('user', userSchema);
